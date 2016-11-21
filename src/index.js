191663cd70eb97d1ab1e95b58257262ba5505c98
yapcheahshen <yapcheahshen@gmail.com>
2016-11-21 23:03:16 +0800

    ptr/def , note and link decorator

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const ReactDOM=require("react-dom");
 const Accelon=require("accelon2016");
+const {ptr,def,note,link}=require("accelon2016/decorators");
 require("./localestring");
 const Main = Accelon(
   {corpus:"yinshun",appname:"yinshun",
     address:"59p9.0301",
     tabs:["source","search","dictionary","config","toc"],
+    decorators:{ptr,def,note,link},
     topMargin:"1.5em",
     leftFlex:3,rightFlex:2
   }
